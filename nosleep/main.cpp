f4353cd80babc5d84b9975aee5a81d32e61c7251
Tobias Dünser <tobias.duenser@gmx.at>
2019-11-22 01:02:56 +0100

    began work on gui

--- nosleep/main.cpp
+++ nosleep/main.cpp
@@ -1,9 +1,89 @@
-#include <Windows.h>
-#include <winbase.h>
+#include <windows.h>
+#include <shellapi.h>
 #include <iostream>
+#include <commctrl.h>
+#include <strsafe.h>
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
-int main() {
-	std::cout << "pc won't enable screensaver while this window is open" << std::endl;
+#define CUSTOM (WM_APP + 100)
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow){
 	SetThreadExecutionState(ES_DISPLAY_REQUIRED | ES_CONTINUOUS);
-	std::cin.get();
+	LPCSTR name = "name";
+	WNDCLASS wc = { };
+
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = name;
+
+	RegisterClass(&wc);
+
+	// Create the window.
+
+	HWND hwnd = CreateWindowEx(
+		0,                              // Optional window styles.
+		name,                     // Window class
+		"Learn to Program Windows",    // Window text
+		WS_OVERLAPPEDWINDOW,            // Window style
+
+		// Size and position
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+		NULL,       // Parent window    
+		NULL,       // Menu
+		hInstance,  // Instance handle
+		NULL        // Additional application data
+	);
+
+	if (hwnd == NULL)
+	{
+		return 0;
+	}
+
+	NOTIFYICONDATA nid = {};
+	nid.hWnd = hwnd;
+	nid.cbSize = sizeof(nid);
+	nid.uFlags = NIF_ICON | NIF_TIP | NIF_GUID | NIF_MESSAGE;
+	nid.uCallbackMessage = CUSTOM;
+	nid.uID = 0;
+
+	// This text will be shown as the icon's tooltip.
+	StringCchCopy(nid.szTip, ARRAYSIZE(nid.szTip), "Test application");
+	// Load the icon for high DPI.
+	LoadIconMetric(NULL, (PCWSTR)MAKEINTRESOURCE(IDI_ERROR), LIM_SMALL, &(nid.hIcon));
+	Shell_NotifyIcon(NIM_ADD, &nid);
+	MSG msg = { };
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case CUSTOM:
+		ShowWindow(hwnd, 1);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hwnd, &ps);
+		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+		EndPaint(hwnd, &ps);
+	}
+	return 0;
+
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
