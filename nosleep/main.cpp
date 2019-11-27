418193a227ca2f81342c452d0c82472e2d4a3b2d
Tobias Dünser <tobias.duenser@gmx.at>
2019-11-27 23:30:57 +0100

    finally have an idea for popup window

--- nosleep/main.cpp
+++ nosleep/main.cpp
@@ -1,10 +1,10 @@
+#include "resource.h"
 #include <windows.h>
 #include <windowsx.h>
 #include <shellapi.h>
 #include <string>
 #include <commctrl.h>
 #include <strsafe.h>
-#include "resource.h"
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 #define ICON (WM_APP + 100)
@@ -17,7 +17,7 @@ HWND parent_window;
 HWND hwnd_text;
 
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow){
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow) {
 	SetThreadExecutionState(ES_DISPLAY_REQUIRED | ES_CONTINUOUS);
 	LPCSTR name = "windowclass";
 	WNDCLASS wc = { };
@@ -75,7 +75,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
 	HWND button1 = CreateWindow(
 		"BUTTON",  // Predefined class; Unicode assumed 
 		"about",      // Button text 
-		 WS_VISIBLE | WS_CHILD | BS_NOTIFY,  // Styles 
+		WS_VISIBLE | WS_CHILD | BS_NOTIFY,  // Styles 
 		0,         // x position 
 		0,         // y position 
 		100,        // Button width
@@ -111,7 +111,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
 
 	TCHAR text[] = "coffee icon by linearicon https://linearicons.com/ \r\n"
 		"creator is https://perxis.com/";
-	
+
 	SendMessage(hwnd_text, WM_SETTEXT, 0, (LPARAM)text);
 	NOTIFYICONDATA nid = {};
 	nid.hWnd = hwnd;
@@ -139,69 +139,88 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
 	return 0;
 }
 
+bool tracking = false;
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-		case ICON:
-		{
-			WORD message_type = LOWORD(lParam);
-			RECT position = {};
-			//get bounding rectangle of clicked icon
-			NOTIFYICONIDENTIFIER icon = {};
-			icon.cbSize = sizeof(NOTIFYICONIDENTIFIER);
-			icon.guidItem = guid;
-			HRESULT result = Shell_NotifyIconGetRect(&icon, &position);
+	case ICON:
+	{
+		WORD message_type = LOWORD(lParam);
+		RECT position = {};
+		//get bounding rectangle of clicked icon
+		NOTIFYICONIDENTIFIER icon = {};
+		icon.cbSize = sizeof(NOTIFYICONIDENTIFIER);
+		icon.guidItem = guid;
+		HRESULT result = Shell_NotifyIconGetRect(&icon, &position);
 
-			if (message_type == WM_RBUTTONDOWN ) {
-				SetWindowPos(hwnd, HWND_TOPMOST, position.left, position.top - 60, 100, 60, SWP_SHOWWINDOW);
-			}
-			return 0;
-		}
-		case WM_COMMAND:
-		{
-			WORD id = LOWORD(wParam);
-			WORD type = HIWORD(wParam);
-
-			if (id == BUTTON_ABOUT) {
-				ShowWindow(parent_window, SW_SHOW);
-			}
-
-			if (id == BUTTON_EXIT) {
-				SendMessage(hwnd, WM_DESTROY, NULL, NULL);
-			}
-			return 0;
-		}
-
-		case WM_DESTROY:
-		{
-			NOTIFYICONDATA nid;
-			nid.cbSize = sizeof(NOTIFYICONDATA);
-			nid.uVersion = NOTIFYICON_VERSION_4;
-			nid.uFlags = NIF_GUID;
-			nid.guidItem = guid;
-			Shell_NotifyIcon(NIM_DELETE, &nid);
-			PostQuitMessage(0);
-			return 0;
-		}
-		case WM_SIZE:
-			// Make the edit control the size of the window's client area. 
-			MoveWindow(hwnd_text,
-				0, 0,                  // starting x- and y-coordinates 
-				LOWORD(lParam),        // width of client area 
-				HIWORD(lParam),        // height of client area 
-				TRUE);                 // repaint window 
-			return 0;
-
-		case WM_PAINT:
-		{
-			PAINTSTRUCT ps;
-			HDC hdc = BeginPaint(hwnd, &ps);
-			FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-			EndPaint(hwnd, &ps);
+		if (message_type == WM_RBUTTONDOWN) {
+			SetWindowPos(hwnd, HWND_TOPMOST, position.left, position.top - 100, 100, 160, SWP_SHOWWINDOW);
 		}
 		return 0;
+	}
+	case WM_MOUSEMOVE:
+	{
+		TRACKMOUSEEVENT track;
+		track.cbSize = sizeof(TRACKMOUSEEVENT);
+		track.dwFlags = TME_HOVER;
+		track.dwHoverTime = 100;
+		track.hwndTrack = hwnd;
+		if (!tracking) {
+			tracking = true;
+			TrackMouseEvent(&track);
+		}
+		return 0;
+	}
+	case WM_MOUSEHOVER:
+	{
+		OutputDebugString("hover");
+		return 0;
+	}
+	case WM_COMMAND:
+	{
+		WORD id = LOWORD(wParam);
+		WORD type = HIWORD(wParam);
 
+		if (id == BUTTON_ABOUT) {
+			ShowWindow(parent_window, SW_SHOW);
+		}
+
+		if (id == BUTTON_EXIT) {
+			SendMessage(hwnd, WM_DESTROY, NULL, NULL);
+		}
+		return 0;
+	}
+
+	case WM_DESTROY:
+	{
+		NOTIFYICONDATA nid;
+		nid.cbSize = sizeof(NOTIFYICONDATA);
+		nid.uVersion = NOTIFYICON_VERSION_4;
+		nid.uFlags = NIF_GUID;
+		nid.guidItem = guid;
+		Shell_NotifyIcon(NIM_DELETE, &nid);
+		PostQuitMessage(0);
+		return 0;
+	}
+	case WM_SIZE:
+		// Make the edit control the size of the window's client area. 
+		MoveWindow(hwnd_text,
+			0, 0,                  // starting x- and y-coordinates 
+			LOWORD(lParam),        // width of client area 
+			HIWORD(lParam),        // height of client area 
+			TRUE);                 // repaint window 
+		return 0;
+
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hwnd, &ps);
+		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+		EndPaint(hwnd, &ps);
+		return 0;
+	}
 	}
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
